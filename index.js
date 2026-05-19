ab4b4a2dc534d5fad6632c8b200c2a24ddca0584
schle1m <230008753+schle1m@users.noreply.github.com>
2026-05-19 21:17:31 +0200

    fix a missmatch

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-async function  random() {
-    const data = await fetch("https://amy-api.onrender.com/api/random")
-    const json = await data.json()
-    const image = json.url;
-    const status = json.status;
-    return {image, status}
+async function random() {
+  const data = await fetch("https://amy-api.onrender.com/api/random");
+  const json = await data.json();
+  const image = json.url;
+  const status = json.status;
+  return { image, status };
 }
-export {random}
+module.exports = { random };
